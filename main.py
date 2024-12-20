645a42bacc9d98e9246e708a815c4cf8fefc3455
fabianomalves <fabianomalves@proton.me>
2024-12-20 11:58:34 -0300

    My first commit for the jornadadedados

---
+++ main.py
@@ -0,0 +1,22 @@
+print('jornadadedados')
+
+# variable
+age = 45
+name = 'Fabiano'
+
+# Print
+print(age)
+
+# Sum
+new_age = age + 1
+print(new_age)
+
+# List
+list_of_numbers = [1, 2, 3, 4, 5]
+print(list_of_numbers)
+
+# Methods
+
+list_of_numbers.reverse()
+
+print(list_of_numbers)
